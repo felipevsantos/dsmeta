5a805412cb12d33ee76f9b6cdd5306c92ca80534
Felipe Veiga <felipeveigadossantos@gmail.com>
2022-07-13 21:17:34 -0300

    Others Components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,22 @@
 import React from "react"
 import NotificationButton from "./components/NotificationButton"
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
 
 
 function App() {
-  return ( 
+  return (
     <>
-  <h1>Olá Mundo!</h1>
-  <NotificationButton />
-  <NotificationButton />
-  <NotificationButton />
-  </>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+
+              <SalesCard/>    
+          </div>
+        </section>
+      </main>
+    </>
   )
 
 }
